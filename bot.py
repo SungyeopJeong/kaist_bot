21ba5b1e107ec0450e6bf6cb8d5141bae8708be3
정성엽 <71690205+SungyeopJeong@users.noreply.github.com>
2022-02-22 14:30:52 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,7 +42,7 @@ def what_is_menu():
           
             source = response.text # menu_info class 내용 가져오기
             soup = BeautifulSoup(source,'html.parser')
-            a = soup.select('.list-1st')
+            a = soup.select('#tab_item_1 > td')
             print(a)
             for menu in a:
                 menu_text = menu.get_text().split()
